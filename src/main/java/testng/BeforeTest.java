fe70d177e8e1b0886dc1efa1de1432991549ebe5
antonmaeso <antonmaeso@hotmail.com>
2021-08-11 21:51:36 +0100

    get data file

--- src/main/java/testng/BeforeTest.java
+++ src/main/java/testng/BeforeTest.java
@@ -1,7 +1,13 @@
 package testng;
-import okwrapper.OKAPIExecutor;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 import org.testng.annotations.*;
+import request.IRequest;
+import request.Request;
+
+import java.nio.file.Paths;
+import java.util.List;
 
 import static org.testng.AssertJUnit.assertEquals;
 
@@ -12,12 +18,20 @@ public class BeforeTest {
     @org.testng.annotations.BeforeTest(alwaysRun = true)
     @Parameters({"requestData", "testVariables"})
     public void beforeTest(String requestData, @Optional String testVariables) {
-        System.out.println(requestData);
-        System.out.println(testVariables);
+        List<IRequest> requests = mapRequests(requestData);
+        System.out.println(new JSONObject(testVariables));
     }
-
-    @Test
-    public void test() {
-        assertEquals(null, json);
+    private List<IRequest> mapRequests(String requestData) {
+        ObjectMapper mapper = new ObjectMapper();
+        if(!requestData.startsWith("\\")) {
+            requestData = "\\" + requestData.replace(".", "\\").replace("\\json", ".json");
+        }
+        try {
+            return mapper.readValue(Paths.get(Paths.get("").toAbsolutePath().toString() + requestData).toFile(), new TypeReference<List<Request>>() {
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 }
