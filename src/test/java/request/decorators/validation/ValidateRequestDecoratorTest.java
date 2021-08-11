adb1f2322192d7f3f5b409209bfdeea003302960
antonmaeso <antonmaeso@hotmail.com>
2021-08-11 20:13:04 +0100

    Update ValidateRequestDecoratorTest.java
    decide what to do about validation

--- src/test/java/request/decorators/validation/ValidateRequestDecoratorTest.java
+++ src/test/java/request/decorators/validation/ValidateRequestDecoratorTest.java
@@ -5,20 +5,21 @@ import request.IRequest;
 import request.Request;
 import request.RequestBuilder;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 
 class ValidateRequestDecoratorTest {
 
     @Test
     void onlyMediaTypeAndNoBody() {
-        Request r = (Request) mock(IRequest.class);
+//        Request r = (Request) mock(IRequest.class);
         IRequest request = new RequestBuilder()
                     .withUrl("https://postman-echo.com/get?foo1=bar1&foo2=bar2")
                     .withMediaType("plain/text")
                     .withMethod("GET")
                     .build();
 //        assertThrows(RequestValidationException.class,() ->{
-//            new ValidateRequestDecorator(r).getBody();
+//           new ValidateRequestDecorator(request).getBody();
 //        });
     }
 
